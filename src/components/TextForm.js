f31696d40e84aea77284ca93afe5028ecfdf8758
Sundaram Sharma <sundaram-sharma@hotmail.com>
2023-01-29 00:16:05 +0530

    Added Lower case button with preview reader

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -9,6 +9,13 @@ export default function TextForm(props) {
     setText(newText);
   }
 
+  const handleLoClick = ()=>{
+    console.log("Uppercase was clicked" + text);
+    setText("You have clicked on handleUpclick");
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
+
   const handleOnChange = (event)=>{ //this is for typing on the form
     console.log("On change");
     setText(event.target.value)
@@ -24,13 +31,17 @@ export default function TextForm(props) {
     <div className="mb-3">
     <label htmlFor="myBox" className="form-label">Example text area</label>
     <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+    
     </div>
-    <button className='btn btn-primary'onClick={handleUpClick}>Convert to upper case</button>
+    <button className='btn btn-primary mx-1'onClick={handleUpClick}>Convert to Upper case</button>
+    <button className='btn btn-primary mx-1'onClick={handleLoClick}>Convert to Lower case</button>
     </div>
-    <div className="container my-2">
+    <div className="container my-3">
       <h1> Your text Summary</h1>
-      <p1> {text.split(" ").length} words and {text.length} charaters </p1>
-
+      <p> {text.split(" ").length} words and {text.length} charaters </p>
+      <p> {0.008 * text.split(" ").length} Minutes Read </p> {/*multiplying the average read time with the number of words*/}
+      <h2>Preview</h2>
+      <p>{text}</p> {/*get the preview of the typed text by user*/}
     </div>
     </>
   )
