8ee32e77a3689cdc83ae71b8ae771542b521113d
Sundaram Sharma <sundaram-sharma@hotmail.com>
2023-01-30 23:53:27 +0530

    Corrected Errors

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -49,13 +49,14 @@ export default function TextForm(props) {
 
   const [text,setText] = useState('');
   //text = "New text"; //Wrong way to change the state
-  //setText("new text"); //Correct way to change the state
+  //setText("new text"); //Correct way to change the state   
+
   return (
     <>
     <div className="container" style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}}>
     <h1 >{props.heading} </h1>
     <div className="mb-3">
-    <label htmlFor="myBox" className="form-label">Example text area</label>
+    <label htmlFor="myBox" className="form-label">Text area</label>
     <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
     
     </div>
@@ -64,7 +65,7 @@ export default function TextForm(props) {
     <button className='btn btn-primary mx-1'onClick={handleClearClick}>Clear Text</button>
     <button className='btn btn-primary mx-1'onClick={handleCapsClick}>Capital first letter</button>
     <button className='btn btn-primary mx-1'onClick={handleCopy}>Copy Text</button>
-    <button className='btn btn-primary mx-1'onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+    <button className='btn btn-primary mx-1'onClick={handleExtraSpaces}>Remove Extra Space</button>
     </div>
     <div className="container my-3" style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}}>
       <h2> Your text Summary</h2>
