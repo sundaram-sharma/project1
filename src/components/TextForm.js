fb03505d369216389d3b41f13b3a40c711b36d2e
Sundaram Sharma <sundaram-sharma@hotmail.com>
2023-02-01 22:48:35 +0530

    Solved Errors

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -60,8 +60,8 @@ export default function TextForm(props) {
     <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
     
     </div>
-    <button className='btn btn-primary mx-1'onClick={handleUpClick}>Convert to Upper case</button>
-    <button className='btn btn-primary mx-1'onClick={handleLoClick}>Convert to Lower case</button>
+    <button className='btn btn-primary mx-1'onClick={handleUpClick}>Convert to Uppercase</button>
+    <button className='btn btn-primary mx-1'onClick={handleLoClick}>Convert to Lowercase</button>
     <button className='btn btn-primary mx-1'onClick={handleClearClick}>Clear Text</button>
     <button className='btn btn-primary mx-1'onClick={handleCapsClick}>Capital first letter</button>
     <button className='btn btn-primary mx-1'onClick={handleCopy}>Copy Text</button>
