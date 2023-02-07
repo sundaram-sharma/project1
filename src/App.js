59fb34bc96b3795eeb16a5208bc3ef6d5110203b
Sundaram Sharma <sundaram-sharma@hotmail.com>
2023-02-08 01:08:30 +0530

    Features added and errors sorted out

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import TextForm from './components/TextForm';
 import About from './components/About';
 import React, {useState} from 'react';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState(`light `); //check the dark mode enabled or not.
@@ -21,29 +27,34 @@ function App() {
   }
 
   const toggleMode = () =>{
-    if(mode === 'dark')
+    if(mode === 'light')
     {
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
       showAlert("Dark mode has been enabled","success");
+      document.title = "TextUtils - Dark Mode"
     }
     else{
-      setMode ('dark');
-      document.body.style.backgroundColor = 'grey';
+      setMode ('light');
+      document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled","success");
+      document.title = "TextUtils - Light Mode"
     }
       
   }
 
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode}/>
-      <div className='container my-3'>
+    <Router>
+      <Navbar title="TextUtils" aboutText={"About"} mode={mode} toggleMode={toggleMode}/>
       <Alert alert={alert}/>
-      <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode}/>
-      <About/>
+      <div className='container my-3'>
+      <Routes>
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode}/>}/>
+          <Route exact path="/about" element={<About/>}/>
+      </Routes>
       </div>
-      
+      </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -60,8 +60,8 @@ export default function TextForm(props) {
     <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
     
     </div>
-    <button className='btn btn-primary mx-1'onClick={handleUpClick}>To Uppercase</button>
-    <button className='btn btn-primary mx-1'onClick={handleLoClick}>To Lowercase</button>
+    <button className='btn btn-primary mx-1'onClick={handleUpClick}>Convert to Uppercase</button>
+    <button className='btn btn-primary mx-1'onClick={handleLoClick}>Convert to Lowercase</button>
     <button className='btn btn-primary mx-1'onClick={handleClearClick}>Clear Text</button>
     <button className='btn btn-primary mx-1'onClick={handleCapsClick}>Capital first letter</button>
     <button className='btn btn-primary mx-1'onClick={handleCopy}>Copy Text</button>
